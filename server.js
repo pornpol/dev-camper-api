8f29ee4bb46ff52c002464838e6796c1dc145675
pornpol <pornpol.w@gmail.com>
2020-02-04 20:19:56 +0700

    Add bootcamps controller & tweak bootcamps router

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-// Import Router
+// Import Routers
 const bootcamps = require("./routes/bootcamps");
 
-// Load env
+// Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-// Mount Router
+// Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
