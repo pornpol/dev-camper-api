fe2b2655a7549fd1e1e765d47d2b1a47d636b7e9
pornpol <pornpol.w@gmail.com>
2020-02-05 09:38:32 +0700

    Add morgar middleware

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const logger = require("./middlewares/logger");
 
 // Import Routers
 const bootcamps = require("./routes/bootcamps");
@@ -8,6 +11,11 @@ const bootcamps = require("./routes/bootcamps");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
