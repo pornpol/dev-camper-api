82af5f7b5f1a8d1a5956fcc151faf9f595252e0b
pornpol <pornpol.w@gmail.com>
2020-02-06 14:29:44 +0700

    Create Slugify & Geocoder middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -39,15 +41,13 @@ const BootcampSchema = new mongoose.Schema({
   location: {
     type: {
       type: String,
-      // required: true,
       enum: ["Point"]
     },
     coordinates: {
       type: [Number],
-      // required: true,
       index: "2dsphere"
     },
-    formatedAddress: String,
+    formattedAddress: String,
     street: String,
     city: String,
     state: String,
@@ -96,6 +96,31 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// Create bootcamp slug for the name
+BootcampSchema.pre("save", function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre("save", async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formatedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
