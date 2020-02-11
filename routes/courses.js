c0fabd80a98ff5a457e925a4f9fde7869cd10ac2
pornpol <pornpol.w@gmail.com>
2020-02-11 19:15:34 +0700

    Create course for specific bootcamp

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
 // Import bootcamps controller
-const { getCourses } = require("../controllers/courses");
+const { getCourses, getCourse, addCourse } = require("../controllers/courses");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getCourses);
+router
+  .route("/")
+  .get(getCourses)
+  .post(addCourse);
+router.route("/:id").get(getCourse);
 
 module.exports = router;
