382a561b001cde4f09c7c79a8d19d87077fe30ba
pornpol <pornpol.w@gmail.com>
2020-02-18 00:34:50 +0700

    Role Authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -27,12 +27,27 @@ exports.protect = asyncHandler(async (req, res, next) => {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log(decoded);
+    // console.log(decoded);
 
-    req.user = await User.findById(decoded._id);
+    req.user = await User.findById(decoded.id);
 
     next();
   } catch (err) {
     return next(new ErrorResponse("Not authorize to access this route", 401));
   }
 });
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
--- models/User.js
+++ models/User.js
@@ -43,7 +43,7 @@ UserSchema.pre("save", async function(next) {
 
 // Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function() {
-  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE
   });
 };
