7ca5424f7c9faca25d9bff94f26860286f20cee5
pornpol <pornpol.w@gmail.com>
2020-02-18 16:15:51 +0700

    Reset Password

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,8 @@ const {
   register,
   login,
   getMe,
-  forgotPassword
+  forgotPassword,
+  resetPassword
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -17,5 +18,7 @@ router.post("/login", login);
 router.get("/me", protect, getMe);
 
 router.post("/forgotpassword", forgotPassword);
+
+router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
