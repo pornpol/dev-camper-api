7946e6cadeae7c159cb3fc80ff320b361a93f2ed
pornpol <pornpol.w@gmail.com>
2020-02-18 16:39:19 +0700

    Update user details and password

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,9 @@ const {
   login,
   getMe,
   forgotPassword,
-  resetPassword
+  resetPassword,
+  updateDetails,
+  updatePassword
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -16,6 +18,10 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.get("/me", protect, getMe);
+
+router.put("/updatedetails", protect, updateDetails);
+
+router.put("/updatepassword", protect, updatePassword);
 
 router.post("/forgotpassword", forgotPassword);
 
